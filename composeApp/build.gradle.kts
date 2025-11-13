ba9fcee655d464467115373e22b3119f71aac7a3
oresidovic <osman.residovic@asseco-see.ba>
2025-11-14 00:36:04 +0100

    KAR-00 builds

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -101,7 +101,7 @@ android {
         applicationId = "karika.distribucija.ba"
         minSdk = libs.versions.android.minSdk.get().toInt()
         targetSdk = libs.versions.android.targetSdk.get().toInt()
-        versionCode = 224
+        versionCode = 225
 
         versionName = "2.2"
     }
