e9a47d568e9cad8c5b1e203bcef0b726137901bb
oresidovic <osman.residovic@asseco-see.ba>
2025-11-20 14:52:19 +0100

    KAR-00 categoryId update

--- composeApp/src/commonMain/kotlin/karika/distribucija/ba/util/KarikaConfig.kt
+++ composeApp/src/commonMain/kotlin/karika/distribucija/ba/util/KarikaConfig.kt
@@ -4,24 +4,13 @@ import karika.distribucija.ba.ui.common.getEnvPrefix
 
 class KarikaConfig {
     companion object {
-        fun getOutletId(): Int =
-            when (getEnvPrefix()) {
-                "test." -> 303
-                "demo." -> 303
-                else -> 303
-            }
+        fun getOutletId(): Int = 303
 
-        fun getActionId(): Int =
-            when (getEnvPrefix()) {
-                "test." -> 301
-                "demo." -> 301
-                else -> 301
-            }
+        fun getActionId(): Int = 301
 
         fun getKarikaProductsId(): Int =
             when (getEnvPrefix()) {
                 "test." -> 363
-                "demo." -> 301
                 else -> 439
             }
     }
