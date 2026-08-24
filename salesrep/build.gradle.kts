9d6a660db9e62b279b3818ff84506bf44b82544a
oresidovic <osman.residovic@asseco-see.ba>
2026-08-24 14:46:21 +0200

    Bump salesrep to versionCode 3 / 0.1.2

--- salesrep/build.gradle.kts
+++ salesrep/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "karika.distribucija.ba.salesrep"
         minSdk = 30
         targetSdk = 37
-        versionCode = 2
-        versionName = "0.1.1"
+        versionCode = 3
+        versionName = "0.1.2"
     }
 
     buildFeatures {
