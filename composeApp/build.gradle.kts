974ae27d0592fbd7b3caff248b1b7e56538f8045
oresidovic <osman.residovic@asseco-see.ba>
2026-08-27 12:30:01 +0200

    KAR-424 - bump version

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -109,9 +109,9 @@ android {
         applicationId = "karika.distribucija.ba"
         minSdk = libs.versions.android.minSdk.get().toInt()
         targetSdk = libs.versions.android.targetSdk.get().toInt()
-        versionCode = 268
+        versionCode = 269
 
-        versionName = "2.6.8"
+        versionName = "2.6.9"
     }
     packaging {
         resources {
